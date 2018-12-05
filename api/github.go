9802e0446938dff1fe59897ce1147a15171cbda3
ankur-anand <ankur.anand12@gmail.com>
2018-12-05 12:28:27 +0530

    issue category

--- api/github.go
+++ api/github.go
@@ -1,13 +1,28 @@
 package api
 
 import (
+	"bufio"
 	"net/http"
+	"strings"
 
 	"github.com/google/go-github/v19/github"
 )
 
 const (
 	newIssue = "opened"
+)
+
+// issueType can be presentaion or request
+type issueType string
+
+var (
+	presentationText = "i'dliketoshowsomething"
+
+	requestText = "i'dliketolearnsomething"
+
+	request      issueType = "request"
+	presentation issueType = "presentation"
+	unknown      issueType = "unknown"
 )
 
 func handleGithubIssueTrigger(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +54,7 @@ func processIssuesEvent(event *github.IssuesEvent) {
 	// action has to be "opened" to take action.
 	action := event.GetAction()
 	if action != newIssue {
+		logger.Info("Not a new opened issue event")
 		return
 	}
 
@@ -62,4 +78,59 @@ func processIssuesEvent(event *github.IssuesEvent) {
 	if len(labels) != 0 {
 		return
 	}
+
+	// get the body of the issue.
+	issueBody := issue.GetBody()
+	scanner := bufio.NewScanner(strings.NewReader(issueBody))
+
+	var firstLine string
+	for scanner.Scan() {
+		firstLine = scanner.Text()
+		// break at first non empty text
+		if firstLine != "" {
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		logger.Error("reading input: " + err.Error())
+	}
+
+	issueIs := isLearnOrRequest(firstLine)
+	logger.Info(issueIs)
+
+}
+
+func isLearnOrRequest(line string) issueType {
+	// check if first line of text is equal to -
+	// presentation topic #I'd Like To Show Something.
+	// or requested topic #I'd Like To Learn Something.
+
+	// trim all space
+	text := strings.TrimSpace(line)
+	// remove all # character ## or ### can be different
+	// better check for occurence of words.
+	text = strings.Trim(text, "#")
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	scanner.Split(bufio.ScanWords)
+
+	var sb strings.Builder
+	for scanner.Scan() {
+		word := scanner.Text()
+		word = strings.ToLower(word)
+		sb.WriteString(word)
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error("reading input: " + err.Error())
+	}
+
+	sbString := sb.String()
+	if strings.Contains(sbString, requestText) {
+		return request
+	}
+	if strings.Contains(sbString, presentationText) {
+		return presentation
+	}
+
+	return unknown
 }
